980705191eb0684a0069e47c0219da45c2f7355e
peterwi <peter@pbwis.com>
2021-02-08 12:35:18 +0000

    String - dictionary

--- HackerRank/HR_ch08/HR_ch08.py
+++ HackerRank/HR_ch08/HR_ch08.py
@@ -5,7 +5,7 @@ my_dict = {i: s.count(i) for i in s}
 
 print(my_dict)
 
-sorted_dict = {k: v for k, v in sorted(my_dict.items(), key=lambda item: item[1], reverse=True)[:3]}
+sorted_dict = {k: v for k, v in sorted(my_dict.items(), key=lambda item: (item[1], item[1]), reverse=True)[:3]}
 
 print(sorted_dict)
 
