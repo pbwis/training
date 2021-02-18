c1b7e7eddf9b766061bfdee9f5c85ea106a3749d
Peter <peter@pbwis.com>
2021-02-18 21:47:32 +0000

    Create Task - part of project

---
+++ Folder/create_task.py
@@ -0,0 +1,23 @@
+# part of project - Wait for  the task to return a result before the main function exist
+import asyncio
+
+
+async def task_func():
+    print('in task_func')
+    return 'the result'
+
+
+async def main():
+    print('creating task')
+    task = loop.create_task(task_func())
+    print('waiting for {!r}'.format(task))
+    return_value = await task
+    print('task completed {!r}'.format(task))
+    print('return value: {!r}'.format(return_value))
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    event_loop.run_until_complete(main(event_loop))
+finally:
+    event_loop.close()
