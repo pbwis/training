f35b87cadd26120f717ac4882eb899d190fe30a6
Peter <peter@pbwis.com>
2021-03-01 16:46:24 +0000

    Anti vowel

---
+++ anti_vowel.py
@@ -0,0 +1,18 @@
+# ANTI VOWEL
+# Function takes one string and return
+# string without vowels
+
+def anti_vowel(text):
+    result = ""
+    vowels = "ieaouIEAOU"
+    for char in text:
+        if char not in vowels:
+            result = result + char
+    return result
+
+
+example = "Hello book"
+print(anti_vowel(example))
+
+# Output:
+# Hll bk
