6b1d6e8a7382201fc532c0bf747f09c2326dc523
Peter <peter@pbwis.com>
2021-03-09 19:58:14 +0000

    Remove duplicates from list, factorial

--- multip_numbers_list.py
+++ multip_numbers_list.py
@@ -14,3 +14,20 @@ print(multiplication(example))
 
 # Output:
 # 100 (4 * 5 * 5)
+
+
+# FACTORIAL
+
+def fact(x):
+    total = 1
+    while x > 0:
+        total *= x
+        x -= 1
+    return total
+
+
+example2 = fact(3)
+print(example2)
+
+# OUTPUT:
+# 6
