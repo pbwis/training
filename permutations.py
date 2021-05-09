55c885ba3b31c93c185c4b2977d3c82ea680a879
Peter <peter@pbwis.com>
2021-05-09 21:41:33 +0100

    permutations

--- permutations.py
+++ permutations.py
@@ -47,3 +47,21 @@ for n, h in enumerate(hands):
 # Show the remaining deck.
 print('\nRemaining deck:')
 show_deck(deck)
+
+# Example of OUTPUT:
+# Initial deck:
+#  2H  2D  2C  2S  3H  3D  3C  3S  4H  4D  4C  4S  5H
+#  5D  5C  5S  6H  6D  6C  6S  7H  7D  7C  7S  8H  8D
+#  8C  8S  9H  9D  9C  9S 10H 10D 10C 10S  JH  JD  JC
+#  JS  QH  QD  QC  QS  KH  KD  KC  KS  AH  AD  AC  AS
+#
+# Hands:
+# 1:  AS  KS  QS  JS 10S
+# 2:  AC  KC  QC  JC 10C
+# 3:  AD  KD  QD  JD 10D
+# 4:  AH  KH  QH  JH 10H
+#
+# Remaining deck:
+#  2H  2D  2C  2S  3H  3D  3C  3S  4H  4D  4C  4S  5H
+#  5D  5C  5S  6H  6D  6C  6S  7H  7D  7C  7S  8H  8D
+#  8C  8S  9H  9D  9C  9S
