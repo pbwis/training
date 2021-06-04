c358f220cc790e01eec2473274c1cba4e788120f
Peter <peter@pbwis.com>
2021-06-04 20:52:07 +0100

    HackerRank task (lists)

---
+++ HackerRank/HR_ch15/HR_ch15.py
@@ -0,0 +1,23 @@
+# HackerRank task
+# Consider a list (list = []). You can perform the following commands:
+# insert i e: Insert integer  at position .
+# print: Print the list.
+# remove e: Delete the first occurrence of integer .
+# append e: Insert integer  at the end of the list.
+# sort: Sort the list.
+# pop: Pop the last element from the list.
+# reverse: Reverse the list.
+
+
+if __name__ == '__main__':
+    N = int(input())
+    l = []
+    for _ in range(N):
+        s = input().split()
+        cmd = s[0]
+        args = s[1:]
+        if cmd !="print":
+            cmd += "("+ ",".join(args) +")"
+            eval("l."+cmd)
+        else:
+            print(l)
